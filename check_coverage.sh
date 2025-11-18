c8187ffbffacf2bd97c2025b7fbd97a699fae2fc
Carole Roch <1rochcarole@gmail.com>
2025-11-18 14:51:19 +0100

    Mettre à jour check_coverage.sh avec fail-under pour le multiplateforme

--- check_coverage.sh
+++ check_coverage.sh
@@ -16,18 +16,6 @@ $PYTHON -m coverage run -m pytest
 
 # Générer le rapport texte
 echo "Generating coverage report..."
-$PYTHON -m coverage report
+$PYTHON -m coverage report --fail-under=80
 
-# Récupérer le pourcentage de couverture de la dernière ligne
-COV=$($PYTHON -m coverage report | tail -n1 | awk '{print $4}' | sed 's/%//' | awk '{printf "%d\n",$1}')
-REQUIRED=80
-
-
-# Vérifier si la couverture est suffisante
-compare=$(echo "$COV >= $REQUIRED" | bc)
-if [ "$compare" -ne 1 ]; then
-  echo "Coverage is $COV%, required is $REQUIRED%"
-  exit 1
-fi
-
-echo "Coverage OK: $COV%"
+echo "Coverage threshold met"
